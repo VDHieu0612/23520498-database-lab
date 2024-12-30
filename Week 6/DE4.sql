e14fba34e2e5ca90f7b9b46f47b558e13acb17af
VDHieu0612 <voduyhieu0612@gmail.com>
2024-12-30 22:15:42 +0700

    Add files via upload

---
+++ Week 6/DE4.sql
@@ -0,0 +1,110 @@
+﻿CREATE DATABASE DE4
+
+USE DE4
+
+CREATE TABLE KHACHHANG (
+    MaKH CHAR(5) NOT NULL CONSTRAINT PK_KH PRIMARY KEY,
+    HoTen VARCHAR(30),
+    DiaChi VARCHAR(30),
+    SoDT VARCHAR(15),
+    LoaiKH VARCHAR(10)
+);
+
+CREATE TABLE BANG_DIA (
+    MaBD CHAR(5) NOT NULL CONSTRAINT PK_BD PRIMARY KEY,
+    TenBD VARCHAR(25),
+    TheLoai VARCHAR(25)
+);
+
+CREATE TABLE PHIEUTHUE (
+    MaPT CHAR(5) NOT NULL CONSTRAINT PK_PT PRIMARY KEY,
+    MaKH CHAR(5) CONSTRAINT FK_PT_KH FOREIGN KEY REFERENCES KHACHHANG(MaKH),
+    NgayThue SMALLDATETIME,
+    NgayTra SMALLDATETIME,
+    Soluongthue INT
+);
+
+CREATE TABLE CHITIET_PM (
+    MaPT CHAR(5) CONSTRAINT FK_CT_PT FOREIGN KEY REFERENCES PHIEUTHUE(MaPT),
+    MaBD CHAR(5) CONSTRAINT FK_CT_BD FOREIGN KEY REFERENCES BANG_DIA(MaBD),
+    CONSTRAINT PK_CTPM PRIMARY KEY (MaPT, MaBD)
+);
+
+--2.1. Thể loại băng đĩa chỉ thuộc các thể loại sau “ca nhạc”, “phim hành động”, “phim tình cảm”, “phim hoạt hình”.
+ALTER TABLE BANG_DIA
+ADD CONSTRAINT CK_TheLoai CHECK (TheLoai IN (N'ca nhạc', N'phim hành động', N'phim tình cảm', N'phim hoạt hình'));
+
+--2.2. Chỉ những khách hàng thuộc loại VIP mới được thuê với số lượng băng đĩa trên 5
+GO
+CREATE TRIGGER trg_CheckSoluongThue_AfterInsertUpdate
+ON PHIEUTHUE
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    -- Kiểm tra nếu khách hàng không phải VIP mà số lượng thuê > 5
+    IF EXISTS (
+        SELECT 1
+        FROM INSERTED i
+        INNER JOIN KHACHHANG kh ON i.MaKH = kh.MaKH
+        WHERE i.Soluongthue > 5 AND kh.LoaiKH <> 'VIP'
+    )
+    BEGIN
+        -- Báo lỗi nếu vi phạm
+        RAISERROR ('Chỉ khách hàng loại VIP mới được thuê với số lượng băng đĩa trên 5.', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END;
+
+GO
+CREATE TRIGGER trg_CheckLoaiKH_AfterUpdate
+ON KHACHHANG
+AFTER UPDATE
+AS
+BEGIN
+    -- Kiểm tra nếu thay đổi loại khách hàng và có phiếu thuê với số lượng > 5
+    IF EXISTS (
+        SELECT 1
+        FROM PHIEUTHUE pt
+        INNER JOIN INSERTED i ON pt.MaKH = i.MaKH
+        WHERE i.LoaiKH <> 'VIP' AND pt.Soluongthue > 5
+    )
+    BEGIN
+        -- Báo lỗi nếu vi phạm
+        RAISERROR ('Khách hàng có phiếu thuê với số lượng băng đĩa trên 5 phải là VIP.', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END;
+
+--3.1. Tìm các khách hàng (MaDG,HoTen) đã thuê băng đĩa thuộc thể loại phim “Tình cảm” có số lượng thuê lớn hơn 3
+SELECT DISTINCT KH.MaKH, KH.HoTen
+FROM KHACHHANG KH
+	 INNER JOIN PHIEUTHUE PT ON KH.MaKH = PT.MaKH
+	 INNER JOIN CHITIET_PM CT ON PT.MaPT = CT.MaPT
+	 INNER JOIN BANG_DIA BD ON CT.MaBD = BD.MaBD
+WHERE BD.TheLoai = N'phim tình cảm' AND PT.Soluongthue > 3;
+
+--3.2. Tìm các khách hàng(MaDG,HoTen) thuộc loại VIP đã thuê nhiều băng đĩa nhất
+SELECT KH.MaKH, KH.HoTen
+FROM KHACHHANG KH
+	 INNER JOIN PHIEUTHUE PT ON KH.MaKH = PT.MaKH
+WHERE KH.LoaiKH = 'VIP'
+GROUP BY KH.MaKH, KH.HoTen
+HAVING SUM(Soluongthue) = (SELECT SUM(Soluongthue)
+						   FROM KHACHHANG KH
+								INNER JOIN PHIEUTHUE PT ON KH.MaKH = PT.MaKH
+						   WHERE KH.LoaiKH = 'VIP'
+						   GROUP BY KH.MaKH, KH.HoTen
+						   ORDER BY SUM(Soluongthue) DESC)
+
+--3.3 Trong mỗi thể loại băng đĩa, cho biết tên khách hàng nào đã thuê nhiều băng đĩa nhất. 
+SELECT TheLoai, HoTen
+FROM KHACHHANG kh,
+(SELECT bd1.TheLoai, pt1.MaKH
+ FROM BANG_DIA bd1, PHIEUTHUE pt1, CHITIET_PM ct1
+ WHERE bd1.MaBD = ct1.MaBD AND ct1.MaPT = pt1.MaPT
+ GROUP BY bd1.TheLoai, pt1.MaKH
+ HAVING COUNT(*) >= ALL (SELECT COUNT(*)
+						 FROM BANG_DIA bD2, PHIEUTHUE pt2, CHITIET_PM ct2
+						 WHERE bd2.MaBD = ct2.MaBD AND ct2.MaPT = pt2.MaPT AND bd2.TheLoai = bd1.TheLoai
+						 GROUP BY pt2.MaKH)) AS THONGKE
+WHERE kh.MaKH = THONGKE.MaKH
---
+++ Week 6/de3.sql
@@ -0,0 +1,95 @@
+﻿CREATE DATABASE DE3
+
+USE DE3
+
+CREATE TABLE DOCGIA (
+    MaDG CHAR(5) NOT NULL CONSTRAINT PK_DG PRIMARY KEY,
+    HoTen VARCHAR(30),
+    NgaySinh SMALLDATETIME,
+    DiaChi VARCHAR(30),
+    SoDT VARCHAR(15)
+);
+
+CREATE TABLE SACH (
+    MaSach CHAR(5) NOT NULL CONSTRAINT PK_SACH PRIMARY KEY,
+    TenSach VARCHAR(25),
+    TheLoai VARCHAR(25),
+    NhaXuatBan VARCHAR(30)
+);
+
+CREATE TABLE PHIEUTHUE (
+    MaPT CHAR(5) NOT NULL CONSTRAINT PK_PT PRIMARY KEY,
+    MaDG CHAR(5) NOT NULL CONSTRAINT FK_PT_DG FOREIGN KEY REFERENCES DOCGIA(MaDG),
+    NgayThue SMALLDATETIME,
+    NgayTra SMALLDATETIME,
+    SoSachThue INT
+);
+
+CREATE TABLE CHITIET_PT (
+    MaPT CHAR(5) NOT NULL CONSTRAINT FK_CTPT FOREIGN KEY REFERENCES PHIEUTHUE(MaPT),
+    MaSach CHAR(5) NOT NULL CONSTRAINT FK_CTMS FOREIGN KEY REFERENCES SACH(MaSach),
+   PRIMARY KEY (MaPT, MaSach)
+);
+
+--2.1. Mỗi lần thuê sách, độc giả không được thuê quá 10 ngày
+ALTER TABLE PHIEUTHUE
+ADD CONSTRAINT CK_ThoiGianThue
+CHECK (DATEDIFF(DAY, NgayThue, NgayTra) <= 10)
+
+--2.2. Số sách thuê bằng tổng số lần thuê trong bảng chi tiết phiếu thuê
+GO
+CREATE TRIGGER TRG_Check_SoSachThue
+ON PHIEUTHUE
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM PHIEUTHUE pt
+        JOIN (
+            SELECT MaPT, COUNT(*) AS TongSoSach
+            FROM CHITIET_PT
+            GROUP BY MaPT
+        ) ct ON pt.MaPT = ct.MaPT
+        WHERE pt.SoSachThue <> ct.TongSoSach
+    )
+    BEGIN
+        RAISERROR ('Số sách thuê không khớp với chi tiết phiếu thuê.', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END;
+
+
+--3.1. Tìm độc giả (MaDG, HoTen) đã thuê sách thể loại "Tin học" trong năm 2007
+SELECT DISTINCT MaDG, HoTen
+FROM DOCGIA
+WHERE MaDG IN (SELECT MaDG
+			   FROM PHIEUTHUE pt 
+					INNER JOIN CHITIET_PT ct ON pt.MaPT = ct.MaPT
+					INNER JOIN SACH s ON ct.MaSach = s.MaSach
+			   WHERE s.TheLoai = 'Tin học' AND YEAR(pt.NgayThue) = 2007)
+
+-- 3.2. Tìm độc giả (MaDG, HoTen) đã thuê nhiều thể loại sách nhất
+SELECT MaDG, HoTen 
+FROM DOCGIA
+WHERE MaDG IN (SELECT MaDG
+			   FROM PHIEUTHUE pt 
+					INNER JOIN CHITIET_PT ct ON pt.MaPT = ct.MaPT
+					INNER JOIN SACH s ON ct.MaSach = s.MaSach
+			   GROUP BY MaDG
+			   HAVING COUNT(DISTINCT s.TheLoai) =  (SELECT TOP 1 COUNT(DISTINCT s.TheLoai)
+													FROM PHIEUTHUE pt 
+														 INNER JOIN CHITIET_PT ct ON pt.MaPT = ct.MaPT
+														 INNER JOIN SACH s ON ct.MaSach = s.MaSach
+													GROUP BY MaDG
+													ORDER BY COUNT(DISTINCT s.TheLoai) DESC))
+
+-- 3.3. Trong mỗi thể loại sách, tìm tên sách được thuê nhiều nhất
+SELECT s1.TheLoai, s1.TenSach, COUNT(*) AS SoLanThue
+FROM SACH s1, CHITIET_PT ct1
+WHERE s1.MaSach = ct1.MaSach
+GROUP BY s1.TheLoai, s1.TenSach
+HAVING COUNT(*) >= ALL (SELECT COUNT(*) 
+						FROM SACH s2, CHITIET_PT ct2
+						WHERE s2.MaSach = ct2.MaSach AND s2.TheLoai = s1.TheLoai
+						GROUP BY s2.MaSach)
